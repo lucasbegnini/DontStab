3ec1b5ceff368a1f84ca4017152fd4b997ff303b
Lucas Begnini <lucasbegnini@gmail.com>
2016-01-30 16:10:59 -0400

    [BEGNINI] Game Over Panel DONE

---
+++ DontStap/Assets/Script/GameOverScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScript : MonoBehaviour {
+
+	public void Reset()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void Menu()
+	{
+		Application.LoadLevel ("menu");
+	}
+}
