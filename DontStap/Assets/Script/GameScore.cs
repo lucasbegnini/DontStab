b4e720bb200fbb6e5473eb109d60aec7467b626e
Lucas Begnini <lucasbegnini@gmail.com>
2016-01-31 11:04:06 -0400

    [Sem o gitinore externo] TESTE

---
+++ DontStap/Assets/Script/GameScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScore : MonoBehaviour {
+
+	private int _score;
+	// Use this for initialization
+	void Start () {
+		_score = 0;
+	}
+
+	public void addScore()
+	{
+		_score ++;
+	}
+
+	public void SaveScore()
+	{
+		if (PlayerPrefs.GetInt ("BestScore") < _score) 
+		{
+			PlayerPrefs.SetInt("BestScore", _score);
+		}
+
+		PlayerPrefs.SetInt("YourScore", _score);
+	}
+
+
+}
